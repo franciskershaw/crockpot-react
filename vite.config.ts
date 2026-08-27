4cca7ce45ebe2e894904f4830f286568c5db3510
Francis Kershaw <73906024+franciskershaw@users.noreply.github.com>
2026-08-27 19:21:19 +0100

    Initial installations

---
+++ vite.config.ts
@@ -0,0 +1,17 @@
+import babel from "@rolldown/plugin-babel";
+import tailwindcss from "@tailwindcss/vite";
+import react, { reactCompilerPreset } from "@vitejs/plugin-react";
+import { defineConfig } from "vitest/config";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    babel({ presets: [reactCompilerPreset()] }),
+    tailwindcss(),
+  ],
+  test: {
+    environment: "jsdom",
+    setupFiles: "./src/setupTests.ts",
+  },
+});

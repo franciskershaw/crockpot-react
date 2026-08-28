aa74726080cacfbc35fada0de51de129997051c4
Francis Kershaw <73906024+franciskershaw@users.noreply.github.com>
2026-08-28 10:56:14 +0100

    Set up tanstack query

---
+++ src/app/App.tsx
@@ -0,0 +1,20 @@
+function App() {
+  return <></>;
+}
+
+export default App;
+
+/**
+ * TODO
+ function App() {
+  return (
+    <BrowserRouter>
+      <AuthProvider>
+        <ToastProvider>
+          <AppRoutes />
+        </ToastProvider>
+      </AuthProvider>
+    </BrowserRouter>
+  );
+}
+ */
--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
+import TanstackQueryProvider from "./lib/Tanstack/TanstackQueryProvider.tsx";
+
 import "./index.css";
 
-import App from "./App.tsx";
+import App from "./app/App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <TanstackQueryProvider>
+      <App />
+    </TanstackQueryProvider>
   </StrictMode>,
 );

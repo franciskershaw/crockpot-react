d99dafad2c93891eef7cb9361b3bad3a1e5a8dbc
Francis Kershaw <73906024+franciskershaw@users.noreply.github.com>
2026-09-04 17:42:53 +0100

    missing ui components

--- src/components/ui/badge.tsx
+++ src/components/ui/badge.tsx
@@ -1,17 +1,25 @@
 import * as React from "react";
 import { cn } from "@/lib/utils";
 import { cva, type VariantProps } from "class-variance-authority";
+import { Slot } from "radix-ui";
 
 const badgeVariants = cva(
-  "inline-flex items-center rounded-full border px-[9px] py-[3px] text-xs font-semibold transition-colors focus:outline-none focus:ring-1 focus:ring-ring",
+  "inline-flex w-fit shrink-0 items-center justify-center gap-1 overflow-hidden rounded-full border border-transparent px-2 py-0.5 text-xs font-medium whitespace-nowrap transition-[color,box-shadow] focus-visible:border-ring focus-visible:ring-[3px] focus-visible:ring-ring/50 aria-invalid:border-destructive aria-invalid:ring-destructive/20 [&>svg]:pointer-events-none [&>svg]:size-3",
   {
     variants: {
       variant: {
-        default: "border-transparent bg-primary text-primary-foreground",
-        secondary: "border-transparent bg-secondary text-secondary-foreground",
-        muted: "border-transparent bg-muted text-muted-foreground",
-        chip: "border-transparent bg-chip text-muted-foreground",
-        outline: "text-foreground",
+        default: "bg-primary text-primary-foreground [a&]:hover:bg-primary/90",
+        secondary:
+          "bg-secondary text-secondary-foreground [a&]:hover:bg-secondary/90",
+        destructive:
+          "bg-destructive text-white focus-visible:ring-destructive/20 [a&]:hover:bg-destructive/90",
+        outline:
+          "border-border text-foreground [a&]:hover:bg-accent [a&]:hover:text-accent-foreground",
+        ghost: "[a&]:hover:bg-accent [a&]:hover:text-accent-foreground",
+        link: "text-primary underline-offset-4 [a&]:hover:underline",
+        muted:
+          "border-transparent bg-muted px-[9px] py-[3px] font-semibold text-muted-foreground",
+        chip: "border-transparent bg-chip px-[9px] py-[3px] font-semibold text-muted-foreground",
       },
     },
     defaultVariants: {
@@ -20,14 +28,22 @@ const badgeVariants = cva(
   },
 );
 
-export interface BadgeProps
-  extends
-    React.HTMLAttributes<HTMLDivElement>,
-    VariantProps<typeof badgeVariants> {}
+function Badge({
+  className,
+  variant = "default",
+  asChild = false,
+  ...props
+}: React.ComponentProps<"span"> &
+  VariantProps<typeof badgeVariants> & { asChild?: boolean }) {
+  const Comp = asChild ? Slot.Root : "span";
 
-function Badge({ className, variant, ...props }: BadgeProps) {
   return (
-    <div className={cn(badgeVariants({ variant, className }))} {...props} />
+    <Comp
+      data-slot="badge"
+      data-variant={variant}
+      className={cn(badgeVariants({ variant }), className)}
+      {...props}
+    />
   );
 }
 
